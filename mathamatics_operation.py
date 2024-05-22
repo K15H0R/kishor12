9ff5858d11f814c3fb15fed88a4a80194e4a731b
KISHOR KUMAR GHALLEY <02230266.cst@edu.bt>
2024-05-22 17:20:26 +0600

    opertaion

---
+++ mathamatics_operation.py
@@ -0,0 +1,12 @@
+name=str(input("Enter name here= "))
+num1=float(input("Enter a number here="))
+num2=float(input("Enter a number here="))
+print("Hello ",name," here are oparations...")
+a=(num1+num2)
+b=(num1-num2)
+c=(num1*num2)
+d=(num1/num2)
+print("addition is",a 
+      ,"subtraction",b,
+      "multiplication",c,
+      "division",d)
